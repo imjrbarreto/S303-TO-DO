b92e8ee0acb3c129c92d93f2b9d6ca1ebb5b3660
CSC <sainz.carolina@gmail.com>
2026-07-23 12:51:30 +0200

    Feat: crear constructor y llamada getTasks en fichero ToDoModel.php

--- app/models/ToDoModel.php
+++ app/models/ToDoModel.php
@@ -4,10 +4,17 @@ require_once 'app/models/StorageInterface.php';
 
 class ToDoModel 
 {
+    private StorageInterface $storage;
 
+    public function __construct(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    } 
 
-
-
+    public function getTasks()
+    {
+        return $this->storage->read();
+    }
 
 
 
