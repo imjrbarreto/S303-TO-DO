5678f4bfce7a340fa55f551158da2213beef54af
CSC <sainz.carolina@gmail.com>
2026-07-23 12:47:03 +0200

    Feat: crear constructor en fichero ToDoController y cambio a minúsculas en carpeta 'todo' de views/scripts

--- app/controllers/ToDoController.php
+++ app/controllers/ToDoController.php
@@ -1,10 +1,21 @@
 <?php
 
 require_once 'app/models/JsonStorage.php';
+require_once 'app/models/ToDoModel.php';
 
 class ToDoController extends Controller
 {
+    public function __construct()
+    {
+        $this->storage = new JsonStorage(ROOT_PATH . 'info.json');
+        $this->todoModel = new ToDoModel($this->storage);
+    }
 
+    public function indexAction()
+    {
+        $tasks = $this->todoModel->getTasks();
+        $this->view->tasks = $tasks; //se envía a la vista
+    }
 
 
 
