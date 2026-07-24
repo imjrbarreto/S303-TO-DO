39759d6222821ad48a69cd0c203fd3f3f2f1a1f1
imjrbarreto <imjrbarreto@gmail.com>
2026-07-24 09:43:50 +0200

    feat: model logic created

--- app/models/ToDoModel.php
+++ app/models/ToDoModel.php
@@ -9,5 +9,22 @@ class ToDoModel
         $this->storage = $storage; 
     }
 
+        public function addTask(string $name, string $description, string $owner)
+    {
+        $data = $this->storage->read();
+        $newData = [
+            "id" => uniqid(),
+            "name" => $name, 
+            "description" => $description,
+            "status" => 'pendiente',
+            "owner" => $owner,
+            "startDate" => (new DateTime())->format('Y-m-d H:m:s'),
+            "endDate" => NULL
+        ];
+
+        $data[] = $newData;
+
+        $this->storage->write($data);
+    }
 }
 ?>
