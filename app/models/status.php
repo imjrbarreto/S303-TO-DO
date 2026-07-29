d13a8564a11442b4e07934630ae047aa44ba943d
CSC <sainz.carolina@gmail.com>
2026-07-29 12:55:41 +0200

    Style: update style of status and some fonts

--- app/models/status.php
+++ app/models/status.php
@@ -5,4 +5,15 @@ enum status: string
     case PENDING = 'pending';
     case INPROCESS = 'in process';
     case COMPLETE = 'complete';
+
+    public function setColor(): string
+    {
+            return match($this) {
+            self::PENDING => "text-red-700",
+            self::INPROCESS => "text-purple-700",
+            self::COMPLETE => "text-emerald-700"
+        };
+
+    }
+    
 }
